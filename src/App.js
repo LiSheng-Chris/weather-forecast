e48f83dac0a7fdf1a5f72caeff52329a2417a902
Li Sheng <lisheng0714@gmail.com>
2023-10-30 00:20:15 +0800

    Add date picker & time picker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import dayjs from "dayjs";
+import "./App.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 
 function App() {
+  const [date, setDate] = React.useState(dayjs(new Date()));
+  const [time, setTime] = React.useState(dayjs(new Date()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8 }}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            sx={{ pr: 2 }}
+            value={date}
+            onChange={(newValue) => setDate(newValue)}
+          />
+          <TimePicker value={time} onChange={(newValue) => setTime(newValue)} />
+        </LocalizationProvider>
+      </Box>
+    </Container>
   );
 }
 
