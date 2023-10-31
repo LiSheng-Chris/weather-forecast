859c2566660fc304de8d688d4d8dd5e1a24c22fd
Li Sheng <lisheng0714@gmail.com>
2023-11-01 01:27:29 +0800

    ReadMe and simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders location", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const location = screen.getByText(/Location/i);
+  expect(location).toBeInTheDocument();
 });
